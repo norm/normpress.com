bdc1403e331c92281fe88c7360b143c7dbd28945
Mark Norman Francis <norm@201created.com>
2021-08-20 14:13:39 +0100

    Extract normpress.com terraform to public repo

---
+++ README.markdown
@@ -0,0 +1,4 @@
+normpress.com
+=============
+
+This is just a silly joke to make one of my friends happy.
---
+++ terraform/main.tf
@@ -0,0 +1,36 @@
+resource "aws_route53_zone" "normpress" {
+  name    = "normpress.com"
+
+  tags = {
+    "Project" = "normpress"
+  }
+}
+
+resource "aws_s3_bucket" "normpress" {
+  bucket  = "normpress"
+
+  tags = {
+    "Project" = "normpress"
+  }
+
+  website {
+    redirect_all_requests_to = "https://github.com/norm/flourish"
+  }
+}
+
+resource "aws_route53_record" "apex_normpress" {
+  name = "normpress.com"
+  type = "A"
+
+  alias {
+    name                   = aws_s3_bucket.normpress.website_domain
+    zone_id                = aws_s3_bucket.normpress.hosted_zone_id
+    evaluate_target_health = false
+  }
+
+  zone_id = aws_route53_zone.normpress.zone_id
+}
+
+output "nameservers" {
+  value = aws_route53_zone.normpress.name_servers
+}
